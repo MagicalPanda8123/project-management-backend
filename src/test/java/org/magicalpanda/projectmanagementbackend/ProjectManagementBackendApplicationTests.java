35a6bfbbfafc96789bf437d90aa154d56b91b972
Blaine Nguyen <m.khang8123@gmail.com>
2026-01-09 10:32:42 +0700

    chore: initial commit

---
+++ src/test/java/org/magicalpanda/projectmanagementbackend/ProjectManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.magicalpanda.projectmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
